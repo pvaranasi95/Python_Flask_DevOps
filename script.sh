2a14dfc90887bc94a9b200ada7f1095045068f70
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-08-22 05:56:28 +0000

    Added script for deploy.yml editing

---
+++ script.sh
@@ -0,0 +1,12 @@
+image=$1
+tag=$2
+
+if [ $# -ne 2 ]; then
+   echo "Insufficient args"
+   exit
+fi
+cd K8S
+ex_image=$(grep image: deploy.yml | awk '{print $2}')
+echo $ex_image
+sed -i "s|$ex_image|$image:$tag|g" deploy.yml
+cat deploy.yml
