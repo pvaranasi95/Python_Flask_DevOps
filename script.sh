4a2521e2dff3b58da0ac5996d096599fc6235fbe
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-08-22 11:52:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,30 @@
+if [ $# -ne 2 ]; then
+    echo "Insufficient args"
+    exit 1
+fi
+
 image=$1
 tag=$2
 
 git checkout -b "feature/flaskapp_$tag"
 
-if [ $# -ne 2 ]; then
-   echo "Insufficient args"
-   exit
-fi
+echo "Image = $image"
+echo "Tag   = $tag"
 
-echo "Image= " $image
-echo "Tag  = " $tag
-cd K8S
-ex_image=$(grep image: deploy.yml | awk '{print $2}')
-echo $ex_image
+cd K8S || exit 1
+
+ex_image=$(grep 'image:' deploy.yml | awk '{print $2}')
+
+echo "Existing image = $ex_image"
+
 sed -i "s|$ex_image|$image:$tag|g" deploy.yml
+
 cat deploy.yml
+
 git config user.name "$GIT_USERNAME"
 git config user.email "jenkins@localhost"
-git add .
+
+git add deploy.yml
 git commit -m "Changed image in deploy.yml file"
-git push --set-upstream origin feature/flaskapp_$tag
+
+git push --set-upstream origin "feature/flaskapp_$tag"
