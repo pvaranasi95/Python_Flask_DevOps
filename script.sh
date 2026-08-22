091188c061d01eac4bdc2b686243129e38b33715
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-08-22 11:54:42 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ sed -i "s|$ex_image|$image:$tag|g" deploy.yml
 
 cat deploy.yml
 
-git config user.name "$GIT_USERNAME"
+git config user.name "pvaranasi95"
 git config user.email "jenkins@localhost"
 
 git add deploy.yml
